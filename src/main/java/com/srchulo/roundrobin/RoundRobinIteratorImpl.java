8624922c2b1ee2d12d4f3900308638a1f983035d
Adam Hopkins <violapiratejunky@gmail.com>
2017-11-04 18:24:38 -0700

    Add RoundRobinIteratorImpl

---
+++ src/main/java/com/srchulo/roundrobin/RoundRobinIteratorImpl.java
@@ -0,0 +1,60 @@
+package com.srchulo.roundrobin;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+
+final class RoundRobinIteratorImpl<T> implements RoundRobinIterator<T> {
+    private final RoundRobinKeyValueIterator<T, T> roundRobinKeyValueIterator =
+            RoundRobinKeyValueIterator.newInstance();
+
+    RoundRobinIteratorImpl() {
+        this(ImmutableList.of());
+    }
+
+    RoundRobinIteratorImpl(Collection<T> collection) {
+        Preconditions.checkNotNull(collection).forEach(this::add);
+    }
+
+    @Override
+    public void add(T value) {
+        roundRobinKeyValueIterator.add(value, value);
+    }
+
+    @Override
+    public void remove(T value) {
+        roundRobinKeyValueIterator.remove(value);
+    }
+
+    @Override
+    public int size() {
+        return roundRobinKeyValueIterator.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return roundRobinKeyValueIterator.isEmpty();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return roundRobinKeyValueIterator.hasNext();
+    }
+
+    @Override
+    public T next() {
+        return roundRobinKeyValueIterator.next();
+    }
+
+    @Override
+    public void remove() {
+        roundRobinKeyValueIterator.remove();
+    }
+
+    @Override
+    public String toString() {
+        return "RoundRobinIteratorImpl{" +
+                "roundRobinKeyValueIterator=" + roundRobinKeyValueIterator +
+                '}';
+    }
+}
